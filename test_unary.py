606159da1caabe1d31df743c5cb64bd437950087
xqq <xqq@xqq.im>
2023-01-03 00:33:12 +0900

    test_unary: Add test for boolean Not

--- test_unary.py
+++ test_unary.py
@@ -4,6 +4,10 @@ from pywasmjit import wasmjit
 def test_add(x: int, y: int) -> int:
     a = x + y
     result = -a
+    b = False
+    c = not b
+    if c:
+        result = -a
     return result
 
 
