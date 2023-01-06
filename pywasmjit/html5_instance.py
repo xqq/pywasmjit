5583fe78de26bcd383629de71b459cad1acf9336
xqq <xqq@xqq.im>
2023-01-07 04:16:58 +0900

    html5_instance: Add implementations

--- pywasmjit/html5_instance.py
+++ pywasmjit/html5_instance.py
@@ -1,9 +1,45 @@
+import pyodide
+import js
+from js import Uint8Array
+from js import WebAssembly
+
 from .exec_instance import ExecInstance
+
+
+def print_int(x: int) -> None:
+    print(x)
+
+def print_float(x: float) -> None:
+    print(x)
+
+def print_bool(x: int) -> None:
+    b = bool(x)
+    print(b)
 
 
 class HTML5Instance(ExecInstance):
     def __init__(self, buf: bytes):
         super().__init__(buf)
 
+        import_funcs = {
+            'js': {
+                'print_int': pyodide.create_proxy(print_int),
+                'print_float': pyodide.create_proxy(print_float),
+                'print_bool': pyodide.create_proxy(print_bool)
+            }
+        }
+
+        self._import_object = pyodide.ffi.to_js(import_funcs, dict_converter=js.Object.fromEntries)
+
+        jsbuf = Uint8Array.new(len(buf))
+        jsbuf.assign(buf)
+
+        self._module = WebAssembly.Module.new(jsbuf)
+        self._instance = WebAssembly.Instance.new(self._module, self._import_object)
+
     def exec_function(self, func_name, *args):
-        pass
+        wasm_func = getattr(self._instance.exports, func_name)
+        if wasm_func is None:
+            raise RuntimeError(f'Function \'{func_name}\' not found in runtime')
+        result = wasm_func(*args)
+        return result
