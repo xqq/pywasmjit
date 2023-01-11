1f1f7987380a6de907f1129289a0011c5ba45258
xqq <xqq@xqq.im>
2023-01-11 22:36:54 +0900

    utils: Move pytype_to_wasmtype into utils

--- pywasmjit/utils.py
+++ pywasmjit/utils.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from .wasm.components import WASMType
+
 DEBUG = False
 
 def debug_print(fmt: str, *args):
@@ -7,9 +11,20 @@ def debug_print(fmt: str, *args):
     print(fmt, *args)
 
 
+def pytype_to_wasmtype(pytype: Optional[str]) -> Optional[WASMType]:
+    if pytype == 'int' or pytype == 'bool':
+        return WASMType('i32')
+    elif pytype == 'float':
+        return WASMType('f64')
+    elif pytype is None or pytype == 'None':
+        return None
+    else:
+        raise RuntimeError(f'Unsupported type: {pytype}')
+
+
 class FunctionSignature:
     __slots__ = ['params', 'return_type']
 
-    def __init__(self, params: list[str], return_type: str):
-        self.params: list[str] = params
-        self.return_type: str = return_type
+    def __init__(self, params: list[str | WASMType], return_type: str | WASMType):
+        self.params: list[str | WASMType] = params
+        self.return_type: str | WASMType = return_type
